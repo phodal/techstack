4189a832f75117b380d658234a73aa808aa77e54
Fengda HUANG <h@phodal.com>
2015-12-22 21:18:51 +0800

    replace with require.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+require.config({
+  baseUrl: 'app',
+  paths: {
+    jquery: 'lib/jquery-2.1.3',
+    json: 'lib/json',
+    d3: 'lib/d3.min',
+    text: 'lib/text'
+  },
+  'shim': {}
+});
+
+require([], function () {
+
+});
