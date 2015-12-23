a1ac9495b97bcd277155b47a41bb3c593bbbebdf
Fengda HUANG <h@phodal.com>
2015-12-23 13:43:32 +0800

    Add Screenshot

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# techstack
+# 项目技术栈趋势图
+
+![Screenshot](screenshot.jpg)
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,7 +24,8 @@ require(['scripts/render', 'json!data/data.json', 'jquery'], function (render, d
       };
 
       function entry(quadrant, position, direction) {
-         var angle = Math.random();
+        var randArray = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95];
+        var angle = randArray[Math.floor(Math.random() * randArray.length)];
         return {
           quadrant: quadrant,
           // position is within the total of horizons.
